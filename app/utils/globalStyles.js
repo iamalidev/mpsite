4e2a5e7eb63876413cc62556e172f9e26fc6b547
alisher <alisherotaboyev072@gmail.com>
2023-07-30 23:40:54 +0500

    project started and font added

---
+++ app/utils/globalStyles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    @font-face {
+        font-family: 'nico-moji-regular';
+        src: url(/fonts/nico-moji.woff2);
+        font-weight: 400;
+    }
+`;
